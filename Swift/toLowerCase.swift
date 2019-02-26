837bcee5a60047b6bbf062b90d7bc6fa48a22b3a
岳杰 <yuejieee@gmail.com>
2019-02-26 18:16:21 +0800

    Add No.709

---
+++ Swift/toLowerCase.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  leetCode
+//
+//  Created by yuejieee on 2019/2/21.
+//  Copyright © 2019 yuejieee. All rights reserved.
+//
+
+import Foundation
+
+// 709. 转换成小写字母
+
+func toLowerCase(_ str: String) -> String {
+    return str.lowercased()
+}
+
+print(toLowerCase("Acbd"))
+
